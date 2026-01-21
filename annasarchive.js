e54382b269c4ac6b35381889be3f014552b6d37b
Clement Denis <clement@01talent.com>
2026-01-21 20:40:08 +0100

    Salute, .org, RIP.

--- annasarchive.js
+++ annasarchive.js
@@ -8,7 +8,7 @@ import {
 } from './lib.js'
 import { updateBook } from './meili.js'
 
-const getAADom = getDom('http://annas-archive.org')
+const getAADom = getDom('http://annas-archive.li')
 
 const isDiv = node => node.tagName === 'DIV'
 const isComment = node => node.nodeName === '#comment'
